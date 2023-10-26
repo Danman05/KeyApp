f10a9daae757176c61ac0053a4179a02c1b7620e
Danman05 <danielspurrell@hotmail.com>
2023-10-26 15:10:02 +0200

    card previews

--- src/app/components/front/front.component.ts
+++ src/app/components/front/front.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Item } from 'src/app/interface/item';
+import { ItemService } from 'src/app/service/item.service';
 
 @Component({
   selector: 'app-front',
   templateUrl: './front.component.html',
   styleUrls: ['./front.component.scss']
 })
-export class FrontComponent {
+export class FrontComponent implements OnInit {
 
+  items: Item[] = []; 
+  constructor(private itemService: ItemService) { }
+
+  ngOnInit(): void {
+    this.itemService.getItems().subscribe(res => {
+      this.items = res.data;
+    });
+  }
 }
