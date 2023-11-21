8b91ad753938b48d7cb4f4ac65825f6873e8946b
Danman05 <danielspurrell@hotmail.com>
2023-11-21 14:24:27 +0100

    updated reservation checking script

--- nodeAPI/scripts/reservationChecker.js
+++ nodeAPI/scripts/reservationChecker.js
@@ -23,14 +23,16 @@ try {
 
 async function fetchData() {
     await db.query(`
-    SELECT vk.virtualKeyId, vk.reserveringId, 
+    SELECT vk.virtualKeyId, vk.reserveringId, vk.userId, 
     r.startDato, r.slutDato,
     e.enhedId, e.enhedTitel,
-    b.fornavn, b.mail
+    bOwner.fornavn AS ownerName, bOwner.mail AS ownerMail,
+    bLender.fornavn AS lenderName, bLender.mail AS lenderMail
     FROM virtualkey as vk
     JOIN reservering AS r ON vk.reserveringId = r.reserveringId
     JOIN enhed AS e ON e.enhedId = r.enhedId
-    JOIN bruger AS b ON b.brugerId = e.enhedEjerId 
+    JOIN bruger AS bOwner ON bOwner.brugerId = e.enhedEjerId
+    JOIN bruger as bLender ON bLender.brugerId = vk.userId 
 `).then(res => {
 
         const todayDate = new Date();
@@ -50,8 +52,8 @@ async function fetchData() {
                 console.log(`expired item added to list - KeyId: ${item.virtualKeyId}, reservationId: ${item.reserveringId}, enhedId: ${item.enhedId}`);
             }
             else if (daysDifference == 0) {
-                sendMail(item);
-                console.log(`sent mail to: ${item.mail}`);
+                sendMail(item.lenderMail, item);
+                console.log(`sent mail to: ${item.lenderMail}`);
             }
         });
 
@@ -78,13 +80,20 @@ async function fetchData() {
     });
 }
 
-function sendMail(item) {
+function sendMail(mail, item) {
 
     const mailOptions = {
         from: '',
-        to: item.mail,
+        to: mail,
         subject: 'Din reservering udløber snart',
-        html: `<p>Hej ${item.fornavn}, </p>
+        text: `Hej ${item.lenderName},
+        
+        Din reservation for ${item.enhedTitel} udløber snart. Her er nogle detaljer:
+        
+        Periode: ${new Date(item.startDato).toLocaleDateString("da-DK")} - ${new Date(item.slutDato).toLocaleDateString("da-DK")}
+        enhed ID: ${item.enhedId}`,
+
+        html: `<p>Hej ${item.lenderName}, </p>
          
         <p>Din reservation for <strong>${item.enhedTitel}</strong> udløber snart. Her er nogle detaljer:</p>
 
